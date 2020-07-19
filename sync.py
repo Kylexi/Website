caa9b321fb63c0359e45d030081e913bb50c2c7a
Matthew Campbell <mmcampbell0@gmail.com>
2020-07-19 18:45:19 -0400

    Updated to new site layout, moved old site to a linked page

--- sync.py
+++ sync.py
@@ -4,6 +4,9 @@ s3_str = "aws s3 sync . s3://matthewcampbell.io"
 s3_str_www = "aws s3 sync . s3://www.matthewcampbell.io"
 
 exclude = ' --exclude "*.git*" --exclude "*.py"'
-os.system(s3_str + exclude)
-os.system(s3_str_www + exclude)
+try:
+	os.system(s3_str + exclude)
+	os.system(s3_str_www + exclude)
+except Exception as e:
+	print(e)
 print("S3 sync successful!")
